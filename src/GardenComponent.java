a6076445409748ba158922daa9f38ad93058a14d
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-02-23 13:54:25 -0600

    Created composition hierarchy

---
+++ src/GardenComponent.java
@@ -0,0 +1,19 @@
+
+public abstract class GardenComponent {
+	
+	public void add(GardenComponent gc)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(GardenComponent gc)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public GardenComponent getChild(int i)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+}
