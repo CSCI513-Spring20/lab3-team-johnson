d928f157be8d2a74c87fb443b441e828dfaa6236
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-02-23 13:33:50 -0600

    Created GardenLayout.java

---
+++ src/GardenLayout.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class GardenLayout extends Application 
+{
+	AnchorPane myPane = new AnchorPane();
+	Scene scene = new Scene(myPane, 50*10, 50*10);
+
+	@Override
+	public void start(Stage gardenStage) throws Exception 
+	{
+		gardenStage
+		
+	}
+	
+	public static void main(String args[])
+	{
+		launch(args);
+	}
+
+}
